0f252570d5aa3c700783a25b06276d3dc2731943
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-08 20:44:24 +0530

    dynamic resturant list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import ReactDom from 'react-dom/client';
 import './App.css';
 import logo from '../src/assets/download.png';
 import res1 from '../src/assets/res1.jpeg';
+import re2 from '../src/assets/re2.jpeg';
+import re3 from '../src/assets/re3.jpeg';
+import re4 from '../src/assets/re4.jpeg';
+import re5 from '../src/assets/re5.jpeg';
+import re6 from '../src/assets/re6.jpeg';
+import res7 from '../src/assets/res7.jpeg';
+import re8 from '../src/assets/re8.jpeg';
+
 
 const Header = () => {
   return (
@@ -26,24 +34,91 @@ const styleCard={
   backgroundColor: 'lightgray',
 }
 
-const ResturantCard = () => {
+const ResturantCard = (props) => {
+  const {resData}=props;
+  const {image,name,cuisines,rating,time}=resData;
   return (
     <div className="res-card" style={styleCard}>
-      <img className='res-img1' src={res1} alt="res1" />  
-      <h3>Megna Foods</h3>
-      <h4>Biriyan Shop , North , New Delhi</h4>
-      <h4>4.4 Rating</h4>
-      <h4>38 minutes</h4>
+      <img className='res-img1' src={image} alt="res1" />  
+      <h3>{name}</h3>
+      <h4>{cuisines}</h4>
+      <h4>{rating}</h4>
+      <h4>{time}</h4>
     </div>
   );
 };
+
+const resList = [
+  {
+    name: "Ocean Breeze",
+    cuisines: "Seafood, Continental",
+    rating: 4.5,
+    time: "30-40 mins",
+    image: res1
+  },
+  {
+    name: "Spicy Delight",
+    cuisines: "Indian, Asian",
+    rating: 4.3,
+    time: "25-35 mins",
+    image: re2
+  },
+  {
+    name: "The Pizza House",
+    cuisines: "Italian, Pizza",
+    rating: 4.7,
+    time: "20-30 mins",
+    image: re3
+  },
+  {
+    name: "Grill Master",
+    cuisines: "American, BBQ",
+    rating: 4.4,
+    time: "35-45 mins",
+    image: re4
+  },
+  {
+    name: "Sushi World",
+    cuisines: "Japanese, Sushi",
+    rating: 4.8,
+    time: "25-35 mins",
+    image: re5
+  },
+  {
+    name: "Taco Fiesta",
+    cuisines: "Mexican, Street Food",
+    rating: 4.2,
+    time: "20-30 mins",
+    image: re6
+  },
+  {
+    name: "Green Leaf Cafe",
+    cuisines: "Vegan, Healthy",
+    rating: 4.5,
+    time: "30-40 mins",
+    image:res7
+  },
+  {
+    name: "Burger Hub",
+    cuisines: "Fast Food, American",
+    rating: 4.1,
+    time: "15-25 mins",
+    image: re8
+  }
+];
+
 
 const Body = () => {
   return (
     <div className="body">
       <div className='search'>Search</div>
       <div className='res-container'>
-        <ResturantCard />
+        {
+          resList.map((resturant)=>(
+            <ResturantCard resData={resturant} key={resturant.name} />
+          ))
+        }
+        
       </div>
     </div>
   );
