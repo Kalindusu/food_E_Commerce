1c46c97962a2ce4128e0f87ab04477742ec6fc29
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-09 22:57:45 +0530

    add filter

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ResturantCard from './ResturantCard';
 import resList from '../utils/mockData';
-
 const Body = () => {
     return (
       <div className="body">
-        <div className='search'>Search</div>
+        <div className='filter'>
+          <button className="filter-btn">Top Rated Resturants</button>
+        </div>
         <div className='res-container'>
           {
             resList.map((resturant)=>(
@@ -14,7 +15,7 @@ const Body = () => {
           }
           
         </div>
-      </div>
+      </div>  
     );
   }
 
